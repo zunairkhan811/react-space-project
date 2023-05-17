8158e7b88ec7af0d1f54d484d5386b1fdf05174f
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-05-18 00:43:19 +0500

    Render Rocket data

--- src/components/Rocket.jsx
+++ src/components/Rocket.jsx
@@ -1,9 +1,26 @@
-const Rockets = () => (
-  <>
-    <div>
-      <h1>Hello world</h1>
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+
+const Rockets = () => {
+  const [reserve, setReserve] = useState('Reserve Rocket');
+  const data = useSelector((state) => state.rockets);
+  console.log(data.rockets);
+  return (
+    <div className="card-container">
+      {data.rockets.map((item) => (
+        <div id={item.rocket_id} key={item.rocket_id} className="card-item">
+          <div className="rocket-image">
+            <img src={item.flickr_images[0]} alt="rocketimg" />
+          </div>
+          <div className="rocket-info">
+            <h1>{item.rocket_name}</h1>
+            <p>{item.description}</p>
+            <button type="button">{reserve}</button>
+          </div>
+        </div>
+      ))}
     </div>
-  </>
-);
+  );
+};
 
 export default Rockets;
