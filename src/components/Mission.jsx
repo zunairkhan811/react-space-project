393c4bcd23d6ae0138b1c451c14de6b3eda35370
rosemutai <chepngetichrose2030@gmail.com>
2023-05-17 08:14:16 +0300

    Display mission data in a table

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,16 +1,37 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchMissions } from '../redux/mission/missionSlice';
+import { useSelector } from 'react-redux';
 
 const Mission = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchMissions());
-  }, []);
-
+  const { missions } = useSelector((state) => state.missions);
+  console.log(missions);
   return (
     <div className="all-missions">
-      <h2>Missions</h2>
+      <table className="missions-table">
+        <thead className="table-header">
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th className="dot">.</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions
+            && missions.map((mission) => (
+              <tr key={mission.mission_id} className="mission-row">
+                <td className="mission-name">{mission.mission_name}</td>
+                <td className="description">{mission.description}</td>
+                <td>
+                  <button type="button" className="not-amember">NOT A MEMBER</button>
+                </td>
+                <td>
+                  <button type="button" className="join-mission">
+                    JOIN A MISSION
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 };
