77ebf9c2a1aebbd305fa5795db697c9f375766f5
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-05-18 16:21:10 +0500

    fix linter errors

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -7,32 +7,35 @@ const MyProfile = () => {
   ));
   return (
     <>
-    <div className="my-profile">
-      <div className="my-missions">
-        <h2 className="missions-title">My Missions</h2>
-        <table className="missions-table">
-          <tbody>
-            {missions && missions.map((mission) => (
-              <tr key={mission.id}>
-                <td>{mission.mission_name}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className="my-profile">
+        <div className="my-missions">
+          <h2 className="missions-title">My Missions</h2>
+          <table className="missions-table">
+            <tbody>
+              {missions && missions.map((mission) => (
+                <tr key={mission.id}>
+                  <td>{mission.mission_name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
       <div className="profile-container">
-      <h1>My Rockets</h1>
-      <ul className="rockets-lists">
-        {data.rockets
-          .filter((item) => item.reserved)
-          .map((item) => (
+        <h1>My Rockets</h1>
+        <ul className="rockets-lists">
+          {data.rockets
+            .filter((item) => item.reserved)
+            .map((item) => (
 
-            <li key={item.id}>{item.rocket_name}</li>
+              <li key={item.id}>{item.rocket_name}</li>
 
-          ))}
-      </ul>
+            ))}
+        </ul>
+      </div>
     </>
-)
-    
+
+  );
+};
 
 export default MyProfile;
