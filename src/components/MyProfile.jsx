036b5f58676cc0831a688a13d8fcb8347c625e47
rosemutai <chepngetichrose2030@gmail.com>
2023-05-18 12:18:18 +0300

    Display my reserved missions

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,5 +1,36 @@
-const MyProfile = () => {
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions } from '../redux/mission/missionSlice';
 
+const MyProfile = () => {
+  const missions = useSelector((state) => state.missions.filter(
+    (mission) => mission.reserved === true,
+  ));
+  console.log(missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!missions.length) dispatch(fetchMissions());
+  }, [dispatch]);
+
+  console.log(missions.reserved);
+  return (
+    <div className="my-profile">
+      <div className="my-missions">
+        <h2 className="missions-title">My Missions</h2>
+        <table className="missions-table">
+          <tbody>
+            {missions && missions.map((mission) => (
+              <tr key={mission.id}>
+                <td>{mission.mission_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="my-rockets" />
+    </div>
+  );
 };
 
 export default MyProfile;
