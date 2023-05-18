a6a461c41efcda7c1f08797a3484f4cc93f9d828
rosemutai <chepngetichrose2030@gmail.com>
2023-05-18 09:17:25 +0300

    Implement Mission leaving

--- src/components/Rocket.jsx
+++ src/components/Rocket.jsx
@@ -1,9 +1,29 @@
-const Rockets = () => (
-  <>
-    <div>
-      <h1>Hello world</h1>
+import { useSelector, useDispatch } from 'react-redux';
+import { reserveRocket } from '../redux/rocket/rocketSlice';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.rockets);
+  //   console.log(data.rockets);
+  return (
+    <div className="card-container">
+      {data.rockets.map((item) => (
+        <div id={item.rocket_id} key={item.rocket_id} className="card-item">
+          <div className="rocket-image">
+            <img src={item.flickr_images[0]} alt="rocketimg" />
+          </div>
+          <div className="rocket-info">
+            <h1>{item.rocket_name}</h1>
+            <p>
+              <span><button type="button" className={!item.reserved ? 'c-reserve' : 'd-reserve'}>Reserved</button></span>
+              {item.description}
+            </p>
+            <button type="button" className="reserve-btn" onClick={() => dispatch(reserveRocket(item.id))}>{!item.reserved ? 'Reserve Rocket' : 'Cancel Reservation'}</button>
+          </div>
+        </div>
+      ))}
     </div>
-  </>
-);
+  );
+};
 
 export default Rockets;
