be6b829dba6e692368ac784363f4409a3f12b1d4
rosemutai <chepngetichrose2030@gmail.com>
2023-05-19 09:10:05 +0300

    Test Navbar component

---
+++ src/__tests__/Navbar.test.js
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+describe('Navbar', () => {
+  it('renders Navbar correctly', () => {
+    const { container } = render(
+      <Router>
+        <Navbar />
+      </Router>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
