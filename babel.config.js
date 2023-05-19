eee54d8b2b9ecfbbea840706623a7cd6766e5042
rosemutai <chepngetichrose2030@gmail.com>
2023-05-19 09:11:42 +0300

    Configure babel

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: ['@babel/preset-env',
+    ['@babel/preset-env', {
+      targets: { esmodules: false, node: 'current' },
+    }],
+    '@babel/preset-flow',
+
+  ],
+  plugins: [
+    [
+      '@babel/plugin-transform-modules-commonjs',
+    ],
+    ['@babel/plugin-proposal-decorators',
+      { legacy: true },
+    ],
+    ['@babel/plugin-proposal-class-properties'],
+  ],
+};
